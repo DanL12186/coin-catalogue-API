1ea6e6b56a39074b569f1a3df10febe496368d63
DanL12186 <DanL12186@users.noreply.github.com>
2020-06-28 18:24:51 -0500

    handle mintmark better and add designer+pcgs pop

--- app/controllers/coins_controller.rb
+++ app/controllers/coins_controller.rb
@@ -3,11 +3,12 @@ class CoinsController < ApplicationController
   def show
     # year, mintmark = params[:year_and_mintmark].split('-')
     year = params[:year_and_mintmark].to_i
-    mintmark = params[:year_and_mintmark].match(/(?<=\-)\D+$/i).to_s.titlecase
+    mintmark = params[:year_and_mintmark].match(/(?<=\-)\D+\d*$/i).to_s.titlecase
+    mintmark = "CC" if mintmark.match?(/^cc/i)
     mintmark = nil if mintmark.empty?
 
     coin = Coin.find_by(year: year, mintmark: mintmark, denomination: params[:denomination])
-
+    # binding.pry
     if coin
       render json: coin
     else
@@ -17,14 +18,14 @@ class CoinsController < ApplicationController
 
   #returns a subset of coins based on query params
   def filter_coins
-    result = Coin.select(:year, :mintmark, :denomination, :category, :mintage, :generic_img_url, :metal_composition, :pcgs_num, :id, :series)
+    result = Coin.select(:year, :mintmark, :denomination, :category, :mintage, :generic_img_url, :metal_composition, :pcgs_num, :id, :series, :designer, :pcgs_population)
                  .where(coin_params.reject { | _, v | v.empty? })
     
     #render json: CoinSerializer.new(result, { fields: { coin: [:year, :mintmark] } })
     if result.empty?
       render status: 404
     else
-      render json: CoinSerializer.new(result).serializable_hash[:data]
+      render json: CoinSerializer.new(result).serializable_hash[:data].map! { | coin | coin[:attributes] }
     end
   end
 
--- app/serializers/coin_serializer.rb
+++ app/serializers/coin_serializer.rb
@@ -1,4 +1,4 @@
 class CoinSerializer
-  include FastJsonapi::ObjectSerializer
-  attributes :id, :year, :mintmark, :denomination, :category, :mintage, :generic_img_url, :metal_composition, :pcgs_num, :series
+  include JSONAPI::Serializer
+  attributes :id, :year, :mintmark, :denomination, :category, :mintage, :generic_img_url, :metal_composition, :pcgs_num, :series, :designer, :pcgs_population
 end
