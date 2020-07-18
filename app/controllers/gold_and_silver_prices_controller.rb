37ebe4174af088c3ea32e9144d725159e4d991e5
DanL12186 <DanL12186@users.noreply.github.com>
2020-07-18 14:15:50 -0500

    change #update_prices from a controller action to a private method

--- app/controllers/gold_and_silver_prices_controller.rb
+++ app/controllers/gold_and_silver_prices_controller.rb
@@ -4,38 +4,41 @@ class GoldAndSilverPricesController < ApplicationController
   
   def prices
     if GoldAndSilverPrice.prices.updated_at.before?(1.day.ago)
-      update_prices
+      render json: update_prices
     else
       render json: GoldAndSilverPrice.prices
     end
   end
 
-  #should only be called if gold and prices were last updated more than 24 hours ago
-  def update_prices
-    api_response = URI.open("https://metals-api.com/api/latest?access_key=#{ENV['GOLD_AND_SILVER_API_KEY']}&base=USD&symbols=XAU,XAG")
+  private
+    def update_prices
+      api_response = Thread.new {
+        URI.open("https://metals-api.com/api/latest?access_key=#{ENV['METALS_API_KEY']}&base=USD&symbols=XAU,XAG")
+      }.value
+    
+      response_hash = JSON.parse(api_response.read)
 
-    response_hash = JSON.parse(api_response.read)
+      if response_hash['success']
+        gold_price     = (1 / response_hash['rates']['XAU']).round(2)
+        silver_price   = (1 / response_hash['rates']['XAG']).round(2)
+        retrieval_date = Date.parse(response_hash['date'])
 
-    if response_hash['success']
-      gold_price     = (1 / response_hash['rates']['XAU']).round(2)
-      silver_price   = (1 / response_hash['rates']['XAG']).round(2)
-      retrieval_date = Date.parse(response_hash['date'])
+        GoldAndSilverPrice.update(gold: gold_price, silver: silver_price, date_retrieved: retrieval_date)
 
-      GoldAndSilverPrice.update(gold: gold_price, silver: silver_price, date_retrieved: retrieval_date)
+        prices = GoldAndSilverPrice.first
 
-      prices = GoldAndSilverPrice.first
-
-      Rails.cache.write("gold_and_silver_prices#{Time.now.day}", prices)
-      
-      render json: prices
-    else
-      begin 
-        error = response_hash['error']
-        raise StandardError("Error Code #{error['code']}: #{error['type']} - #{error['info']}")
-      rescue StandardError => error
-        puts "drat"
+        Rails.cache.write("gold_and_silver_prices#{Time.now.day}", prices)
+        
+        return prices
+      else
+        begin 
+          error = response_hash['error']
+          raise StandardError("Error Code #{error['code']}: #{error['type']} - #{error['info']}")
+        rescue StandardError => error
+          puts "drat"
+          return GoldAndSilverPrice.prices
+        end
       end
     end
-  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-  root 'application#home'
-
   get '/coins' => 'coins#filter_coins'
   get '/coin' => 'coins#show'
 
   get '/series' => 'series#filter_series'
 
   get '/prices' => 'gold_and_silver_prices#prices'
-  get '/update_prices' => 'gold_and_silver_prices#update_prices'
 end
