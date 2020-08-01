72eb477f7533a69f6b8c80777dee7e259ee26352
DanL12186 <DanL12186@users.noreply.github.com>
2020-08-01 10:16:10 -0500

    add basic user to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_09_203927) do
+ActiveRecord::Schema.define(version: 2020_08_01_034711) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,6 +68,13 @@ ActiveRecord::Schema.define(version: 2020_07_09_203927) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "denomination", default: "", null: false
     t.string "reverse_img_url", default: ""
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
