6254a71359345ce0807632b20dcec1ccd56bbf06
DanL12186 <DanL12186@users.noreply.github.com>
2020-08-01 22:29:04 -0500

    add basic validations and user tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  validates :name, presence: true, uniqueness: true
+  validates :password, length: { minimum: 8 }
+
+  # has_many :wishlists
+  # has_many :coin_collections
+
+  # has_many :wishlist_coins, through: :wishlists, source: :coins
+  # has_many :coins, through: :coin_collections
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  context 'validation tests' do 
+    it 'ensures password presence' do
+      user = User.new(name: "My Name")
+      
+      expect(user.save).to eq(false)
+    end
+
+    it "ensures password length >= 8" do
+      user = User.new(name: "My Name", password: "7letter")
+
+      expect(user.save).to eq(false)
+    end
+
+    it 'allows user to be created with a username and valid password' do
+      user = User.new(name: "My Name", password: "8letters")
+
+      expect(user.save).to eq(true)
+    end
+  end
+
 end
