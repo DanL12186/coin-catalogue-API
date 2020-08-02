4ce9e120512993f36a21ac9ba79d5c27eb547e3d
DanL12186 <DanL12186@users.noreply.github.com>
2020-08-02 14:49:23 -0500

    add basic coin controller tests

---
+++ spec/controllers/coins_controller_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe CoinsController, type: :controller do
+  let(:valid_coin) { 
+    Coin.create(year: 1907, denomination: '$20', series: "Liberty Head Double Eagle", mintmark: "D", special_designation: "", category: "Double Eagles") 
+  }
+
+  describe 'validation tests' do
+    context 'GET #show' do
+    
+      context 'with empty params' do
+        it 'returns a 404 not found response' do
+          get :show
+
+          expect(response.status).to eq(404)
+        end
+      end
+
+      context 'with valid params' do
+        it 'returns a 200 success response in JSON format' do
+          get :show, params: valid_coin.attributes
+
+          expect(response).to be_successful
+          expect(response.content_type).to eq "application/json; charset=utf-8"
+        end
+      end
+      
+    end
+  end
+
+end
