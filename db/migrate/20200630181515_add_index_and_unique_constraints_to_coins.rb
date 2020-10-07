88538e6f9f0d60600363cf0b9b69ccc2ade26bc9
DanL12186 <DanL12186@users.noreply.github.com>
2020-10-07 15:42:38 -0500

    remove dead migrations

--- db/migrate/20200630181515_add_index_and_unique_constraints_to_coins.rb
+++
@@ -1,8 +0,0 @@
-class AddIndexAndUniqueConstraintsToCoins < ActiveRecord::Migration[6.0]
-  def change
-    add_index :coins, 
-              [:series, :year, :mintmark, :special_designation],
-              unique: true,
-              name: 'index_coins_on_series_year_mintmark_and_special_designation'
-  end
-end
--- db/migrate/20200704202325_remove_index_and_unique_constraint_from_coins.rb
+++
@@ -1,5 +0,0 @@
-class RemoveIndexAndUniqueConstraintFromCoins < ActiveRecord::Migration[6.0]
-  def change
-    remove_index :coins, name: "index_coins_on_series_year_mintmark_and_special_designation"
-  end
-end
